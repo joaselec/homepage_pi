fa2d090348982bda924cfcb3f688e6b366f9f620
khj <ki8107@gmail.com>
2025-04-10 17:50:53 +0900

    커밋 메시지

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
-from flask import Flask, render_template
+# from flask import Flask, render_template
 
-app = Flask(__name__, 
-            template_folder='app/templates',
-            static_folder='app/static')
+# app = Flask(__name__, 
+#             template_folder='app/templates',
+#             static_folder='app/static')
+
+# @app.route('/')
+# def home():
+#     return render_template('index.html')
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+from flask import Flask
+
+app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return "Hello from Flask!"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0')
